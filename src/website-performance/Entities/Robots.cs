ba09179001fea12560519bbd8070e889df32aa14
João Rosa <joaoasrosa@gmail.com>
2020-05-25 16:07:03 +0200

    Add ctor to pipe the http factory

--- src/website-performance/Entities/Robots.cs
+++ src/website-performance/Entities/Robots.cs
@@ -1,14 +1,26 @@
 ﻿using System;
 using System.Collections.Generic;
+using website_performance.Infrastructure;
 
 namespace website_performance.Entities
 {
     public class Robots
     {
+        private readonly IHttpMessageHandlerFactory _httpMessageHandlerFactory;
         private readonly List<Uri> _sitemaps = new List<Uri>();
 
         public Robots(string url)
         {
+            if (Uri.TryCreate(url, UriKind.Absolute, out var robotsUrl))
+                Url = robotsUrl;
+            else
+                throw new UriFormatException($"Fail to parse URL \"{url}\"");
+        }
+        
+        public Robots(string url, IHttpMessageHandlerFactory httpMessageHandlerFactory)
+        {
+            _httpMessageHandlerFactory = httpMessageHandlerFactory ?? throw new ArgumentNullException(nameof(httpMessageHandlerFactory));
+            
             if (Uri.TryCreate(url, UriKind.Absolute, out var robotsUrl))
                 Url = robotsUrl;
             else
